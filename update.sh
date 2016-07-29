aa73cd77f5e736672d7bd98eb10007e10a33ca18
Paweł Golonko <pgolonko@gmail.com>
2016-07-29 16:39:10 +0200

    Configuration scripts

---
+++ update.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+dir="$HOME/.dotfiles"
+
+# our config files/dirs to link
+paths="config/fish config/fisherman vim vimrc wakatime.cfg"
+
+########
+
+# backup dir for old files
+backup=.backup/
+
+# make sure we are in the right place
+cd "$dir"
+
+# make backup dir
+echo "Creating backup dir for the old existing dotfiles..."
+if [ -d "$backup" ]; then
+  rm -rf "$backup/*"
+else
+  mkdir "$backup"
+fi
+
+# move files to the backup dir, then create symlinks
+for path in $paths; do
+  dest="$HOME/.$path"
+
+  if [ ! -d $path ]; then
+    echo "$path configuration doesn't exists in repository"
+    continue;
+  fi
+
+  if [ -h $dest ]; then
+    echo "~/.$path is already linked"
+    continue
+  fi
+
+  echo "Moving old configuration files to $dir/$backup"
+  mv $dest $backup
+
+  echo "Creating symlink to $path in home directory"
+  ln -s $path $dest
+done
